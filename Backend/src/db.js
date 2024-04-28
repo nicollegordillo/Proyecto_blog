a61be9747710b715d2ab79b3a7b1abe33f94839c
nicollegordillo <nickygordillo912@gmail.com>
2024-04-28 02:26:48 -0600

    Fix: hash

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -12,13 +12,12 @@ export async function login(username, password) {
         }
 
         const user = rows[0];
-        const { password } = user;
 
         // Genera el hash de la contraseña proporcionada junto con la sal almacenada
         const hashedPassword = crypto.createHash('md5').update(password).digest('hex');
 
         // Compara el hash generado con el hash almacenado en la base de datos
-        if (hashedPassword === password) {
+        if (hashedPassword === user.password) {
             return user; // Si los hashes coinciden, retorna los datos del usuario
         } else {
             return null; // Si los hashes no coinciden, retorna null
